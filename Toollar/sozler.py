e916b56102c11951ecfa1e463a42e18b82489da0
Dueclaion <140893853+Dueclaion@users.noreply.github.com>
2023-07-30 18:15:51 +0400

    Add files via upload

---
+++ Toollar/sozler.py
@@ -0,0 +1,63 @@
+salam = (
+  'Salam',
+  'Aleykum Salam',
+  'Salam Balımm',
+  'Salamm Daaaa',
+  'Salam Ömrümmm',
+  'İkı Gözümün Çiçəyi Salam',
+  )
+  
+necesen = (
+    'Yaxşıyamm',
+    'Yaxşıyamm Sən Nəcəsən',
+    'Sa Maraqlı Deyil',
+    'Nəyinə Lazımdı?',
+    'Ömrümmm Yaxşıyamm Sən Yaxşı Ol',
+    'Pisəm',
+    'Beleda Yaşıyırıq',
+    'Ne Sən Soruş, Nə Mən Deyim',
+    )
+    
+geldim = (
+
+      'Xoş Gəldin'
+      'Xoş Gəldın Ömrümm',
+      'Balammm Xoşş',
+      'Gelmesen Yaxşı İdi',
+      'Qayıt Dala',
+      'Maraqli Deyil Get',
+      )
+      
+ayrılan_user = (
+        'Xos Getdin',
+        'Görüm Heç Bide Gelmiyesen',
+        'Sağollaşmadan Hara Getdin Ey',
+        'Seni Çokk Özlüyomm Lütfen Gel',
+        'Həyaaatımm Hara Gettin',
+        'Bide Bura Gelen.....',
+        )
+        
+yeni_user = (
+          'Xoş Geldinnn Üreyimmm',
+          'Balamaa Salammm',
+          'Senin Yolunu Gözleyirdim Ne Vaxtdandı',
+          'Gəlll Ömrümmmm',
+          'Xoş Gəldinnn İkı Gözümün Çiçəyii',
+          )
+          
+ban = (
+            'He Bax Bele Ağıllı Ol',
+            'Qaşınırsann???',
+            'Yetkinn Yoxdu Qurdalamadaa',
+            'Burnun Girmediyi Yerə Başını Soxma',
+            'Əlləmə Botu',
+            )
+            
+Ruslan = (
+              'Sevgilime Dəymə',
+              'Haycan Balımmm',
+              'Canımmm Ruslanımm',
+              'Ruslansız Bir Günümüz Olmasın',
+              'Narahat Eleme',
+              'Sizin Kimi Boş Bekar Deyil O',
+              )

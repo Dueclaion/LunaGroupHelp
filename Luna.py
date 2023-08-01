17464d7a5881d14f62eb90a40ccc7445e42aedfa
Dueclaion <140893853+Dueclaion@users.noreply.github.com>
2023-08-01 09:21:36 +0400

    Update Luna.py

--- Luna.py
+++ Luna.py
@@ -292,6 +292,27 @@ async def song(event):
         print(e)
 
 
+#@edalet_22 terefindən @RoBotlarimTg üçün yazilib silmədən istifadə edin
+@Luna.on(events.NewMessage(pattern="^/id ?(.*)"))
+async def id(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_id = previous_message.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Sizin Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**İstifadəçi id:** `{user_id}`\n**Qrup id:** `{chat_id}`")
+
+
+#@edalet_22 terefindən @RoBotlarimTg üçün yazilib silmədən istifadə edin
+    else:
+        user_id = event.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Sizin Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**İstifadəçi id:** `{user_id}`\n**Qrup id:** `{chat_id}`")
 									
 													
 print("Luna Mükemmel Çalışıyorr")

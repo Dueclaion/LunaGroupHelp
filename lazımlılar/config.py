06508ed302a57bb7ae04236300eba5d72b987eb3
Dueclaion <140893853+Dueclaion@users.noreply.github.com>
2023-08-01 16:12:59 +0400

    Update config.py

--- lazımlılar/config.py
+++ lazımlılar/config.py
@@ -5,7 +5,7 @@ API_ID = 28348304
 API_HASH = "99275a99d9d593fd50ecf6127f0ac312" 
 bot_token = "6079678077:AAGKQjLzvE5a5NiHs8wnA8OvXZ65Bz8FVnE"
 string_session = "1ApWapzMBu1g-oDeolumFkvMqFEh_0GZ7TdxgU1k-491a0itPC4Arw3CWdO-vs8hX2SXE3SoHEn6pxm4KG0fVl7p6vNRtQ0nVBvUlO1G-fMuzqDncIrFwVCWHgCReRwKEmbJHdiOxrXALlXQSGU2vbVfBxVJbz__fmyZu5LWB_hbSQah9FlFoZ88TwwXBOTzHfdxTHiImcoIO4HH-6FlNzHQwZaufYaML_By58e1KUdH1td1YJJFTv8-GOUpvwDf-Sx7vOtlBK5g3fihM0JJCoHBSI0etWNPcdKjsFXJzVeF7hddbcxRxD63SaQzlZcLIIxUzs46ma1s632l2xUEv6fRpV6Usckg="
-admin_qrup = int(os.environ.get(""))
+admin_qrup = int(os.environ.get("-960446527"))
 etiraf_qrup = int(os.environ.get(""))
 kanal = os.environ.get("")
 log_group = int(os.environ.get(""))
